a90c8006511dbed6dcab61e89c96e024de47bf93
SJDavies2020 <44582668+SJDavies2020@users.noreply.github.com>
2019-10-09 22:47:50 -0400

    Building out index/homepage

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/Projects', function (req, res, next) {
+  res.render('Projects', { title: 'Projects' });
+});
+
+router.get('/AboutMe', function (req, res, next) {
+  res.render('AboutMe', { title: 'About Me' });
+});
+
+router.get('/Services', function (req, res, next) {
+  res.render('Services', { title: 'Services' });
+});
+
+router.get('/ContactMe', function (req, res, next) {
+  res.render('ContactMe', { title: 'Contact ME' });
 });
 
 module.exports = router;
